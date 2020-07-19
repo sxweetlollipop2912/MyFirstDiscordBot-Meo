6480d8188dc0ecbdbb0a4c297404d53b9e657300
SxweetLollipop <thaolygoat@gmail.com>
2020-07-19 22:43:58 +0700

    v1.5: Add CF commands

---
+++ 2nd/Commands/BasicCommands/HelpModules.cs
@@ -0,0 +1,154 @@
+﻿using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+
+using Microsoft.Extensions.Configuration;
+
+
+public class HelpMudule : ModuleBase<SocketCommandContext>
+{
+    readonly IConfigurationRoot _config;
+    readonly DiscordSocketClient _client;
+
+    public HelpMudule(IConfigurationRoot config, DiscordSocketClient client)
+    {
+        _config = config;
+        _client = client;
+    }
+
+
+
+    [Command("help", RunMode = RunMode.Async)]
+    [Alias("command", "commands")]
+    [Summary("help")]
+    public async Task Help()
+    {
+        //return;
+
+        var embed = new EmbedBuilder
+        {
+            Description = "**>> Cùng điểm qua những câu lệnh Mèo có nào!**",
+            Color = Color.Magenta
+        };
+        embed.AddField("*'hi*",
+                       "Cùng chào nhau nhé!")
+            .AddField("*'react <ID tin nhắn> <emoji_1> <emoji_2> ... <emoji_n>*",
+                      "Mèo sẽ \"thả tym\" vào tin nhắn có ID đó!")
+            .AddField("*'say <true/false>(true nếu ẩn danh, mặc định là false) <thời gian chờ>(theo giây, mặc định là 0) <tin nhắn>*",
+                      "Mèo sẽ giúp bạn gửi tin nhắn đó!")
+            .AddField("*'party*",
+                      "Đến lúc quẩy rồi!!! :partying_face:")
+            .AddField("*'remind <giờ>(0 - 23) <phút>(0 - 59) <giây>(0 - 59, mặc định là 0) <lời nhắc>*",
+                      "Mèo sẽ nhắc nhở bạn vào thời điểm đó!")
+            .AddField("*'wait <giây> <lời nhắn>*",
+                      "Mèo sẽ gửi một tin nhắn y hệt lời nhắn đó sau số giây nhất định!")
+            .AddField("Ngoài ra, bạn có thể thử *'join*, *'play*, *'stop* và *'leave*~ :notes:",
+                      "Dùng *'help exam* để xem các lệnh về các bài kiểm tra~\n" +
+                      "Dùng *'help game* để xem các lệnh về trò chơi~\n" +
+                      "Dùng *'help CF* để xem các lệnh về Codeforces~\n" +
+                      " *^^Chúc bạn một ngày tốt lành!^^*");
+
+
+        await SendMessage(embed: embed.Build());
+    }
+
+
+
+    [Command("help exam", RunMode = RunMode.Async)]
+    [Alias("command exam", "commands exam")]
+    public async Task HelpExam()
+    {
+        var embedExam = new EmbedBuilder
+        {
+            Color = Color.LightOrange
+        };
+        embedExam.AddField("*>> 'exam*", "**>> Những lệnh liên quan đến các bài kiểm tra trong lớp T.T:**")
+            .AddField("*'exam add <môn học> <thứ>(2 - 6) <thời lượng kiểm tra> <ghi chú>(nếu có)*", "Thêm bài kiểm tra vào danh sách của Mèo~")
+            .AddField("*'exam get <thứ>(2 - 6)*", "Xem tất cả các bài kiểm tra có trong ngày hôm đó.")
+            .AddField("*'exam get <môn học>*", "Xem tất cả các bài kiểm tra của môn học đó.")
+            .AddField("*'exam all*", "Xem tất cả các bài kiểm tra có trong 7 ngày tới!")
+            .AddField("*'exam rm <thứ>(2 - 6)*", "Xoá các bài kiểm tra của ngày đó trong danh sách của Mèo! :partying_face:")
+            .AddField("*'exam rm <môn học> <thứ>(2 - 6) <thời lượng kiểm tra> <ghi chú>(nếu có)*", "Xoá bài kiểm tra trong danh sách của Mèo! :partying_face:")
+            .AddField("*'exam clear*", "Xoá toàn bộ các bài kiểm tra trong danh sách của Mèo!!! :partying_face::tada::confetti_ball:")
+            .AddField("^^Cố gắng lên nào!^^ :fist::fist:", "*^^Chúc bạn làm bài thật tốt!^^*");
+
+        await SendMessage(embed: embedExam.Build());
+    }
+
+
+
+    [Command("help game", RunMode = RunMode.Async)]
+    [Alias("command game", "commands game")]
+    public async Task HelpGame()
+    {
+        var embedChess = new EmbedBuilder
+        {
+            Color = Color.LightOrange
+        };
+        embedChess.AddField("*>> 'chess*",
+                      "**>> Những lệnh liên quan đến trò chơi :chess_pawn:~:**")
+            .AddField("*'chess new <tên người chơi 2>:*",
+                      "Bắt đầu một ván đấu mới.")
+            .AddField("*'chess continue*",
+                      "Tiếp tục ván đấu được lưu (nếu chưa kết thúc).")
+            .AddField("*'chess print*",
+                      "In ra bàn cờ và trạng thái của ván đấu được lưu.")
+            // Move
+            .AddField("*>> move*", "**>> Những lệnh trong ván đấu:**")
+            .AddField("*move <nước đi (vd a2 b3)>*",
+                      "Di chuyển quân cờ.")
+            .AddField("*move pause*",
+                      "Tạm ngưng trò chơi.")
+            .AddField("*move draw*",
+                      "Đề xuất hoà ván đấu.")
+            .AddField("*move surrender*",
+                      "Kết thúc trò chơi với kết quả thua. :cry:")
+            //Puzzle
+            .AddField("*>> 'puzzle*",
+                      "**>> Những lệnh liên quan đến trò chơi :jigsaw:~:**")
+            .AddField("*'puzzle new:*",
+                      "Bắt đầu một trò chơi mới.")
+            .AddField("*'puzzle continue*",
+                      "Tiếp tục trò chơi được lưu (nếu chưa kết thúc).")
+            .AddField("*'chess print*",
+                      "In ra trò chơi đang được lưu.")
+            .AddField("^^Chơi hết mình nhé!^^ :fist::fist:", "*^^Chúc mọi người chơi vui vẻ!^^*");
+
+        await SendMessage(embed: embedChess.Build());
+    }
+
+
+
+    [Command("help CF", RunMode = RunMode.Async)]
+    [Alias("command CF", "commands CF")]
+    public async Task HelpCF()
+    {
+        var embedCF = new EmbedBuilder
+        {
+            Color = Color.Red
+        };
+        embedCF.AddField("*>> 'cf*",
+                         "**>> Những lệnh liên quan Codeforces:**")
+            .AddField("*'cf contest <số lượng, mặc định là 0>",
+                      "Mèo cho bạn danh sách các contest sắp diễn ra, ưu tiên contest diễn ra (nếu số lượng là 0 thì sẽ in tất cả.");
+
+        await SendMessage(embed: embedCF.Build());
+    }
+
+
+    private async Task<RestUserMessage> LogDiscord(string log)
+    {
+        var channel = _client.GetChannel(_config.GetValue<ulong>("guild:Test:log")) as ISocketMessageChannel;
+        return await SendMessage(content: log, Channel: channel);
+    }
+
+    private async Task<RestUserMessage> SendMessage(string content = null, Embed embed = null, ISocketMessageChannel Channel = null)
+    {
+        if (Channel == null) Channel = Context.Channel;
+        await Channel.TriggerTypingAsync();
+        return await Channel.SendMessageAsync(text: content, embed: embed);
+    }
+}
---
+++ 2nd/Commands/StudyCommands/CFModules.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Globalization;
+
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+
+using cfapi.Objects;
+using cfapi.Methods;
+
+using Microsoft.Extensions.Configuration;
+
+public class CFModule : ModuleBase<SocketCommandContext>
+{
+    readonly IConfigurationRoot _config;
+    readonly DiscordSocketClient _client;
+
+    public CFModule(IConfigurationRoot config, DiscordSocketClient client)
+    {
+        _config = config;
+        _client = client;
+    }
+
+    
+    [Command("cf contest", RunMode = RunMode.Async)]
+    [Summary("Print a list of all available contests")]
+    public async Task ContestList(int count = 0)
+    {
+        var contestRequest = new ContestListRequest();
+        var contests = await contestRequest.GetContestListAsync(includeGym: false);
+        contests.Reverse();
+        if (count <= 0) 
+            count = contests.Count();
+
+        var embed = new EmbedBuilder
+        {
+            Description = "***>> List of incoming contests:***",
+            Color = Color.Red
+        };
+
+        foreach (var contest in contests.Where(i => i.Phase == ContestPhase.BEFORE))
+        {
+            if (count == 0)
+                break;
+
+            var Name = contest.Name;
+            var Url = "https://codeforces.com/contests/" + contest.Id.ToString();
+            var Type = contest.Type;
+            var StartTime = DateTimeOffset.FromUnixTimeSeconds(contest.StartTime).AddHours(7);
+            var DurationMin = (int)TimeSpan.FromSeconds(contest.Duration).TotalMinutes;
+            var Author = contest.Author;
+
+            string StartDate = $"*{DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(StartTime.Month)}/{StartTime.Day}/{StartTime.Year}* at {StartTime.Hour}:{((StartTime.Minute < 10) ? $"0{StartTime.Minute}" : $"{StartTime.Minute}")} UTC+7";
+            string Duration = $"{(int)(DurationMin / 60)}h " + ((DurationMin % 60 < 10) ? $"0{DurationMin % 60}m" : $"{DurationMin % 60}m");
+
+            embed.AddField($"{StartDate}",
+                           $"[{Name}]({Url})\nLength: {Duration}\nAuthor: {Author}");
+            --count;
+        }
+
+        await SendMessage(embed: embed.Build());
+    }
+
+
+    private async Task<RestUserMessage> LogDiscord(string log)
+    {
+        var channel = _client.GetChannel(_config.GetValue<ulong>("guild:Test:log")) as ISocketMessageChannel;
+        return await SendMessage(content: log, Channel: channel);
+    }
+
+    private async Task<RestUserMessage> SendMessage(string content = null, Embed embed = null, ISocketMessageChannel Channel = null)
+    {
+        if (Channel == null) Channel = Context.Channel;
+        await Channel.TriggerTypingAsync();
+        return await Channel.SendMessageAsync(text: content, embed: embed);
+    }
+}
